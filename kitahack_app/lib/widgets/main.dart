99b9d6b27d83b37aa348cdd786a1c990521f95d5
falf0005 <falf0005@student.monash.edu>
2026-02-11 14:15:19 +0300

    Initialize Firebase before the app runs

---
+++ kitahack_app/lib/widgets/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'screens/home_screen.dart';
+
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
+  runApp(const ResilienceBuilderApp())
+}
+
+class ResilienceBuilderApp extends StatelessWidget {
+  const ResilienceBuilderApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ResilienceBuilder',
+      theme: ThemeData(
+        // Use a "Warning" color scheme
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.redAccent),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(), // Start at the Map
+    );
+  }
+}
